260678dc6ee5da51ebe969a8d4a386a3afe4962c
Lizardo Perdomo <lizardo@utp.edu.co>
2026-03-15 19:43:55 -0500

    :art: Updated app metadata

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig, loadEnv } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig, loadEnv } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
 
-const { publicVars } = loadEnv({ prefixes: ['VITE_'] });
+const { publicVars } = loadEnv({ prefixes: ["VITE_"] });
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
@@ -10,5 +10,24 @@ export default defineConfig({
     define: {
       ...publicVars,
     },
+  },
+  html: {
+    title: "Streetrack",
+    tags: [
+      {
+        tag: "meta",
+        head: true,
+        attrs: {
+          name: "description",
+          content:
+            "Streetrack is a personal finance / expense-tracking application to manage income and expenses in a structured way.",
+        },
+      },
+      {
+        tag: "meta",
+        head: true,
+        attrs: { name: "application-name", content: "Streetrack" },
+      },
+    ],
   },
 });
